3505e3859e94958700249d34342476751e18ed42
Joe Quattrone <joequattrone5@gmail.com>
2018-10-19 13:21:28 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,2 +1,24 @@
 def my_select(collection)
+  if block_given?
+    i = 0
+    new_array = []
+  
+    while i < array.length
+      new_array.push(yield(array[i]))
+       i += 1
+      end
+     new_array
+=======
+    if block_given?
+    i = 0
+    new_array = []
+    
+      while i < array.length
+       yield(array[i])
+        i += 1
+      end
+      new_array
+    end
+  end
 end
+
