9467dcaef721c4b5fc0b9741e0c2ebe4499afe1f
Joe Quattrone <joequattrone5@gmail.com>
2018-10-19 13:21:45 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -8,17 +8,6 @@ def my_select(collection)
        i += 1
       end
      new_array
-=======
-    if block_given?
-    i = 0
-    new_array = []
-    
-      while i < array.length
-       yield(array[i])
-        i += 1
-      end
-      new_array
-    end
   end
 end
 
